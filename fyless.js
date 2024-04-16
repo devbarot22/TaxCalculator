1b4c445136094fa684fdbf501ff95b5ecc64783b
devbarot22 <dbarot717@gmail.com>
2024-04-16 18:51:35 +0530

    Update Functionality

--- fyless.js
+++ fyless.js
@@ -2,12 +2,14 @@
 
 
 const formFields = [
-  { label: 'Enter gross annual income', placeholder: 'Enter gross annual income', type: 'text', tooltip:'Gross annual income is your total salary in a year before any deductions' },
-  { label: 'Enter extra income', placeholder: 'Enter extra income from other sources', type: 'text', tooltip:'' },
-  { label: 'Enter age group', placeholder: '', type: 'select', options: ['Select Your Age Group', 'age < 40', 'age ≥ 40 & < 60', 'age ≥ 60'], tooltip:'' },
-  { label: 'Enter total applicable deductions', placeholder: 'Add total applicable deduction', type: 'text', tooltip:''},
+  { label: 'Enter gross annual income', placeholder: 'Enter gross annual income', type: 'text', tooltip: 'Annual income reflects pre-deduction earnings' },
+  { label: 'Enter extra income', placeholder: 'Enter extra income from other sources', type: 'text', tooltip: 'More moolah on top of your salary dough!' },
+  { label: 'Enter age group', placeholder: '', type: 'select', options: ['Select Your Age Group', 'age < 40', 'age ≥ 40 & < 60', 'age ≥ 60'], tooltip: 'Age groups: where tax rates get sassy with the classy!' },
+  { label: 'Enter total applicable deductions', placeholder: 'Add total applicable deduction', type: 'text', tooltip: 'Total deductions: Giving your taxes a break, not your paycheck!' },
   { label: '', type: 'submit' }
 ];
+
+
 
 function formatNumber(num) {
   if (num >= 100000) {
@@ -22,28 +24,82 @@ function formatNumber(num) {
 
 const form = document.getElementById('form');
 
-for (const field of formFields) {
+for (const [index, field] of formFields.entries()) {
 
   if (field.label == "") {
     const submit = document.createElement('input');
     submit.type = field.type;
     submit.className = 'subBtn';
-    submit.name = 'submit'; 
+    submit.name = 'submit';
     submit.value = 'Submit';
     form.appendChild(submit);
   }
   else {
     const label = document.createElement('label');
     label.textContent = field.label;
-    label.className = 'labelField'; 
-    label.innerHTML += `<img class="question-mark" src="assets/question-mark-button-svgrepo-com.svg" alt="info mark" data-tooltip="${field.tooltip}"/>`;
+    label.className = 'labelField';
+
+
+    const tooltipContainer = document.createElement('div');
+    tooltipContainer.className = 'tooltip-container';
+
+    const questionMark = document.createElement('img');
+    questionMark.className = 'question-mark';
+    questionMark.src = 'assets/question-mark-button-svgrepo-com.svg';
+    questionMark.alt = 'info mark';
+
+    const tooltip = document.createElement('div');
+    tooltip.className = `tooltipQ tooltipQ${index + 1}`;
+    tooltip.innerHTML = `<span class="tooltiptextQ" style="background: #36454F">${field.tooltip}</span>`;
+    tooltip.style.display = 'none';
+
+
+
+
+
+    tooltipContainer.appendChild(questionMark);
+    tooltipContainer.appendChild(tooltip);
+
+    label.appendChild(tooltipContainer);
+
+    let tooltipTimeout;
+
+
+    // question mark mouseover event
+
+    questionMark.addEventListener('mouseover', () => {
+      clearTimeout(tooltipTimeout);
+      tooltipTimeout = setTimeout(() => {
+        const rect = questionMark.getBoundingClientRect();
+
+        tooltip.style.top = `${rect.top - tooltip.offsetHeight - 30}px`;
+        tooltip.style.left = `${rect.left + rect.width / 2 - tooltip.offsetWidth / 2}px`;
+        tooltip.style.display = 'block';
+      }, 200);
+    });
+
+
+
+    // question mark mouseout event
+
+
+    questionMark.addEventListener('mouseout', () => {
+      clearTimeout(tooltipTimeout);
+      tooltipTimeout = setTimeout(() => {
+        tooltip.style.display = 'none';
+      }, 200);
+    });
+
+
+
+
 
     const inputWrapper = document.createElement('div');
     inputWrapper.className = "inputparent";
-    
+
     const inputContainer = document.createElement('div');
     inputContainer.className = 'inputConSubParent';
-    inputContainer.style.position = 'relative'; 
+    inputContainer.style.position = 'relative';
     inputContainer.style.width = '100%';
 
     const warning = document.createElement('i');
@@ -60,28 +116,36 @@ for (const field of formFields) {
         optionElement.text = option;
         select.appendChild(optionElement);
       }
-      inputContainer.appendChild(select); 
+      inputContainer.appendChild(select);
     } else {
       const input = document.createElement('input');
       input.type = field.type;
       input.className = "inputFields"
-      input.name = field.label; 
-      input.autocomplete = 'off'; 
+      input.name = field.label;
+      input.autocomplete = 'off';
       input.placeholder = field.placeholder;
-      inputContainer.appendChild(input); 
+      inputContainer.appendChild(input);
     }
 
-    inputContainer.appendChild(warning); 
-    inputWrapper.appendChild(inputContainer); 
+    inputContainer.appendChild(warning);
+    inputWrapper.appendChild(inputContainer);
     form.appendChild(label);
     form.appendChild(inputWrapper);
   }
 }
 
-window.onload = function() {
+
+
+
+// excla mark warning will block on input's except number. 
+
+
+
+
+window.onload = function () {
   var inputFields = document.querySelectorAll('.inputFields[type=text]');
-  inputFields.forEach(function(inputField) {
-    inputField.addEventListener('input', function() {
+  inputFields.forEach(function (inputField) {
+    inputField.addEventListener('input', function () {
       var exclaMark = this.nextElementSibling;
       var value = this.value;
 
@@ -97,6 +161,9 @@ window.onload = function() {
 
 // Functionality and calculator building
 
+
+
+// for user convenience parsing m and k for million and thousand.
 
 function parseIncome(incomeStr) {
   let income = parseFloat(incomeStr);
@@ -127,74 +194,103 @@ function calculateTax(income, extraIncome, deductions, ageGroup) {
   return tax;
 }
 
-form.addEventListener('submit', function(event) {
+
+
+
+
+// modal which pops-up to show the overall Income after tax deduction
+
+form.addEventListener('submit', function (event) {
   event.preventDefault();
 
-  const income = parseIncome(document.querySelector('.inputFields[name="Enter gross annual income"]').value);
-  const extraIncome = parseIncome(document.querySelector('.inputFields[name="Enter extra income"]').value);
-  const deductions = parseIncome(document.querySelector('.inputFields[name="Enter total applicable deductions"]').value);
-  const ageGroup = document.querySelector('.inputFields[name="Enter age group"]').value;
+  const income = parseIncome(document.querySelector('.inputFields[name="Enter gross annual income"]').value) || 0;
+  const extraIncome = parseIncome(document.querySelector('.inputFields[name="Enter extra income"]').value) || 0;
+  const deductions = parseIncome(document.querySelector('.inputFields[name="Enter total applicable deductions"]').value) || 0;
+  const ageGroupField = document.querySelector('.inputFields[name="Enter age group"]');
+  const ageGroup = ageGroupField.value;
+
+  if (ageGroup === 'Select Your Age Group') {
+
+    const tooltip = document.createElement('div');
+    tooltip.className = 'tooltip';
+    tooltip.innerHTML = `
+      <span class="tooltiptext">Please select your age group</span>
+    `;
+
+
+    ageGroupField.parentNode.insertBefore(tooltip, ageGroupField);
+
+
+    tooltip.querySelector('.tooltiptext').style.visibility = 'visible';
+    tooltip.querySelector('.tooltiptext').style.opacity = '1';
+
+    ageGroupField.addEventListener('change', () => {
+      tooltip.style.display = 'none';
+    });
+
+    return;
+  }
 
   const tax = calculateTax(income, extraIncome, deductions, ageGroup);
-  const overallIncome = (income + extraIncome) - (tax * 100000); 
+  const overallIncome = (income + extraIncome) - (tax * 100000);
 
   const modal = createModal();
 
-
   modal.firstChild.innerHTML = `
-  <h1>Your Overall income will be</h1>
-  <h3>${formatNumber(overallIncome.toLocaleString())}</h3>
-  <p>after tax deductions</p>`;
+    <h1>Your Overall income will be</h1>
+    <h3>${formatNumber(overallIncome.toLocaleString())}</h3>
+    <p>after tax deductions</p>`;
 
-  // Create the close button
+
   const closeButton = document.createElement('button');
   closeButton.id = 'close-modal';
   closeButton.innerHTML = `<img src="./assets/x-close-delete-svgrepo-com.svg" class='svg-x' alt="Close">`;
- 
-  closeButton.addEventListener('click', function() {
+
+  closeButton.addEventListener('click', function () {
     modal.style.display = 'none';
   });
 
-  // Append the close button to the modal
+
   modal.firstChild.appendChild(closeButton);
 
-  // Show the modal
   modal.style.display = 'block';
 });
 
 
 
 
+// createModal create the modal
+
 
 
 
 function createModal() {
-  // Create the modal and its elements
+
   const modal = document.createElement('div');
-  const modalContentWrapper = document.createElement('div'); // This is the new wrapper div
+  const modalContentWrapper = document.createElement('div');
   const closeButton = document.createElement('button');
 
-  // Set the attributes and content of the modal and its elements
+
   modal.id = 'modal';
 
-  // Set the attributes and content of the wrapper div
+
   modalContentWrapper.id = 'modalContentWrapper'
 
   closeButton.id = 'close-modal';
   closeButton.innerHTML = `<img src="./assets/x-close-delete-svgrepo-com.svg" alt="Close">`;
   closeButton.style.cursor = 'pointer'
-  
-    // Append the modal content and close button to the modal
-    modalContentWrapper.appendChild(closeButton);
 
-  // Append the wrapper to the modal
+
+  modalContentWrapper.appendChild(closeButton);
+
+
   modal.appendChild(modalContentWrapper);
 
-  // Append the modal to the body
+
   document.body.appendChild(modal);
 
-  // Add an event listener to the close button that hides the modal when clicked
-  closeButton.addEventListener('click', function() {
+
+  closeButton.addEventListener('click', function () {
     modal.style.display = 'none';
   });
 
